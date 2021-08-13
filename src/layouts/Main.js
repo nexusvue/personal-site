6ef2b6ccfc8c05b10b33c2bb2e6d9bf9ffd0f5d1
NexusVue Dev <e.d.istro@gmail.com>
2021-08-13 10:29:22 +1000

    Update Main.js

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo" defer={false}>
+    <Helmet titleTemplate="%s | Cameron McDougall" defaultTitle="Cameron McDougall" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Michael D'Angelo's personal website.",
+  description: "Cameron McDougall's personal website.",
 };
 
 export default Main;
